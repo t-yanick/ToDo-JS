1a848e22b8103be0c1ce9efc2ed8e250b9a1454f
Yanick <tyankee15@gmail.com>
2021-02-25 09:07:56 -0300

    Render projects module as DOM

--- src/render-projects.js
+++ src/render-projects.js
@@ -1,37 +1,167 @@
-const pureFunctions = () => {
-	const fetchLocalStorage = (item) => {
-		let listObject = [];
-		if (
-			localStorage.getItem(item) !== null
-			&& localStorage.getItem(item) !== 'undefined'
-		) {
-			listObject = JSON.parse(localStorage.getItem)
-		} else if (item === 'Projects') {
-			listObject = ['Default Project'];
-		} else {
-			listObject = [];
+import {
+	projectObjectList,
+	createProjects,
+	removeProject,
+} from "./project-object.js";
+import { projectList, storeProjects, storeTodos } from "./storage.js";
+import { createTodos } from "./todo-object.js";
+import { renderToDoObjects } from "./render-todo-objects.js";
+
+const content = document.querySelector("#content");
+
+//Render a project
+const renderProject = (project) => {
+	const projectDiv = document.createElement("div");
+
+	projectDiv.setAttribute("class", "project");
+
+	const projectTitle = document.createElement("p");
+	projectTitle.setAttribute("class", "projectTitle");
+	projectTitle.append(project.title);
+
+	projectDiv.append(projectTitle);
+	projectsListContainer.append(projectDiv);
+
+	const projectRemoveBtn = document.createElement("button");
+	projectRemoveBtn.setAttribute("class", "projectRemoveBtn");
+	projectRemoveBtn.textContent = "X";
+	projectRemoveBtn.addEventListener("click", (e) => {
+		const pageOverlay = document.createElement("div");
+		pageOverlay.setAttribute("id", "pageOverlay");
+
+		pageOverlay.addEventListener("click", (e) => {
+			pageOverlay.remove();
+		});
+
+		//Project delete confirmation prompt
+		const removeProjectCheckContainer = document.createElement("div");
+		removeProjectCheckContainer.setAttribute(
+			"id",
+			"removeProjectCheckContainer"
+		);
+
+		const removeProjectCheckText = document.createElement("p");
+		removeProjectCheckText.textContent =
+			"Are you sure you want to remove this project? \n All todo items will be lost.";
+
+		const removeProjectCheckYes = document.createElement("button");
+		removeProjectCheckYes.setAttribute("class", "removeCheckBtn");
+		removeProjectCheckYes.textContent = "Remove";
+		removeProjectCheckYes.addEventListener("click", (e) => {
+			project.projectTodoList.forEach((i) => {
+				localStorage.removeItem(
+					project.title + " " + i.title + " todo info"
+				);
+			});
+			localStorage.removeItem(project.title + " project todo list");
+			removeProject(project);
+			storeProjects.removeProjectFromList(project);
+			pageOverlay.remove();
+			projectDiv.remove();
+			todoContainer.remove();
+		});
+
+		const removeProjectCheckNo = document.createElement("button");
+		removeProjectCheckNo.setAttribute("class", "removeCheckBtn");
+		removeProjectCheckNo.textContent = "Cancel";
+		removeProjectCheckNo.addEventListener("click", (e) => {
+			pageOverlay.remove();
+		});
+
+		removeProjectCheckContainer.append(
+			removeProjectCheckText,
+			removeProjectCheckYes,
+			removeProjectCheckNo
+		);
+		pageOverlay.append(removeProjectCheckContainer);
+		content.insertAdjacentElement("afterbegin", pageOverlay);
+	});
+
+	projectDiv.append(projectRemoveBtn);
+
+	projectDiv.addEventListener("click", (e) => {
+		const todoContainer = document.querySelector("#todoContainer");
+		if (content.contains(todoContainer)) {
+			todoContainer.remove();
 		}
-		return listObject
-	};
-
-	const saveLocalStorage = (key, itemsList) => {
-		localStorage.setItem(key, JSON.stringify(itemsList));
-	};
-
-	function urlDashedName(name) {
-		return name.split(/\s+/).join('-');
-	}
-
-	function urlUndasedName(name) {
-		return name.split(/-+/).join(' ');
-	}
-
-	return {
-		fetchLocalStorage,
-		saveLocalStorage,
-		urlDashedName,
-		urlUndasedName,
-	};
+		storeTodos.getTodoList();
+		createTodos();
+		renderToDoObjects(project);
+	});
 };
 
-export default pureFunctions;
+//Renders all projects
+const renderProjectList = () => {
+	projectObjectList.forEach((listItem) => {
+		renderProject(listItem);
+	});
+};
+
+const renderProjectCreateBtn = (() => {
+	const projectCreateBtnContainer = document.createElement("div");
+	projectCreateBtnContainer.setAttribute("id", "projectCreateBtnContainer");
+
+	const projectCreateBtn = document.createElement("button");
+	projectCreateBtn.setAttribute("id", "projectCreateBtn");
+	projectCreateBtn.textContent = "Create Project";
+	projectCreateBtn.addEventListener("click", (e) => {
+		projectCreateBtn.remove();
+		const createProjectPopup = document.createElement("div");
+		createProjectPopup.setAttribute("id", "createProjectPopup");
+		projects.insertAdjacentElement("afterbegin", createProjectPopup);
+
+		const projectTitleText = document.createElement("p");
+		projectTitleText.setAttribute("id", "projectTitleText");
+		projectTitleText.textContent = "Title:";
+		createProjectPopup.append(projectTitleText);
+
+		const projectTitleInput = document.createElement("input");
+		projectTitleInput.setAttribute("id", "projectTitleInput");
+		projectTitleInput.setAttribute("placeholder", "New Project");
+		createProjectPopup.append(projectTitleInput);
+
+		const projectExistsError = document.createElement("p");
+		projectExistsError.setAttribute("class", "projectExistsError");
+		projectExistsError.textContent = "Project already exists!";
+
+		const noNameError = document.createElement("p");
+		noNameError.setAttribute("class", "noNameError");
+		noNameError.textContent = "Enter a name!";
+
+		const projectSubmitBtn = document.createElement("button");
+		projectSubmitBtn.textContent = "Save";
+		projectSubmitBtn.setAttribute("class", "btn");
+		projectSubmitBtn.addEventListener("click", (e) => {
+			if (projectList.includes(projectTitleInput.value)) {
+				if (!createProjectPopup.contains(projectExistsError)) {
+					createProjectPopup.insertAdjacentElement(
+						"afterend",
+						projectExistsError
+					);
+					return;
+				}
+			} else if (projectTitleInput.value == "") {
+				createProjectPopup.insertAdjacentElement(
+					"afterend",
+					noNameError
+				);
+				return;
+			} else if (projectTitleInput.value != "") {
+				storeProjects.addProjectToList(projectTitleInput.value);
+				createProjects();
+				createProjectPopup.remove();
+				projectTitleInput.remove();
+				projectSubmitBtn.remove();
+				projectCreateBtnContainer.append(projectCreateBtn);
+				projectsListContainer.innerHTML = "";
+				renderProjectList();
+			}
+		});
+		createProjectPopup.append(projectSubmitBtn);
+	});
+
+	projectCreateBtnContainer.append(projectCreateBtn);
+	projects.insertAdjacentElement("afterbegin", projectCreateBtnContainer);
+})();
+
+export { renderProjectList };
